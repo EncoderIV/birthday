aa4a6d2dff93f90c14325ba612c2aaff74bec741
EncoderIV <145885774+EncoderIV@users.noreply.github.com>
2024-02-19 21:44:38 -0500

    rough addCandle function added

---
+++ index.js
@@ -0,0 +1,11 @@
+const cake = document.getElementsByClassName("cake-image");
+const ageBox = document.getElementsByClassName("age-box-to-display");
+
+let age = 0 ;
+
+// hard coded index 0 cuz won't actually ever have more than 1 cake , so technically should have used id instead of class here
+//cake[0].addEventListener("click" , addCandle);
+
+function addCandle(){
+    age = age + 1 ;
+}
